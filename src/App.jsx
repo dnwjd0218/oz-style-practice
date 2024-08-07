3b8ada44e6ef613ee73fb20fac09cd21eeb4fb2e
dnwjd0218 <dnwjd1325@naver.com>
2024-08-07 17:33:05 +0900

    React 과제 11일차

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import "./App.scss";
+
+import styled from "styled-components";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
+const Section = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  padding: 20px 40px;
+`
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <Section>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </Section>
     </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,33 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    background-color: black;
+`
+const HeaderUl = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+`
+const HeaderLi = styled.li`
+  list-style: none;
+  font-weight: 400;
+  font-size: 13px;
+`
+
 export default function Header() {
   return (
-    <header className="header-container">
+    <HeaderContainer>
       <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
-      </ul>
-    </header>
+      <HeaderUl>
+        <HeaderLi>로그인</HeaderLi>
+        <HeaderLi>회원가입</HeaderLi>
+        <HeaderLi>내클래스</HeaderLi>
+      </HeaderUl>
+    </HeaderContainer>
   );
 }
